4715c08829e9042a558dbb9e5cb9e3778b0e7e9c
Zonotora <zonotora@hotmail.com>
2022-09-17 00:04:15 +0800

    Modify io

--- www/components/io.tsx
+++ www/components/io.tsx
@@ -5,8 +5,8 @@ type Props = {};
 const Io: React.FC<Props> = () => {
   return (
     <div className="io">
-      <div style={{ height: "50vh", backgroundColor: "black" }}></div>
-      <div style={{ height: "50vh", backgroundColor: "white" }}></div>
+      <div style={{ height: "50vh", backgroundColor: "#121212" }}>Input</div>
+      <div style={{ height: "50vh", backgroundColor: "#232323" }}>Output</div>
     </div>
   );
 };
